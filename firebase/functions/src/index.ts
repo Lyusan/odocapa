bb995654da51e6a7df8c1984682443fd029b35ee
lyusan <leo.champion@protonmail.com>
2023-02-28 16:23:23 +0100

    function: clean up and document index.ts

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,7 +4,13 @@ import { JSDOM } from 'jsdom';
 
 const cors = require('cors')({ origin: true });
 
-const streetInfoBoxMap = [
+interface InfoboxMapElement {
+  key: string;
+  frPropNames: string[];
+  type: 'listSplitByComma' | 'time';
+}
+
+const STREET_INFOBOX_MAP = [
   {
     key: 'length',
     frPropNames: ['longueur'],
@@ -30,9 +36,9 @@ const streetInfoBoxMap = [
     frPropNames: ['ancien nom'],
     type: 'listSplitByComma',
   },
-];
+] as InfoboxMapElement[];
 
-const personInfoboxMap = [
+const PERSON_INFOBOX_MAP = [
   {
     key: 'birthday',
     frPropNames: ['date de naissance', 'naissance'],
@@ -55,35 +61,55 @@ const personInfoboxMap = [
   },
   {
     key: 'school',
-    frPropNames: ['Formation'],
+    frPropNames: ['formation'],
     type: 'listSplitByComma',
   },
-];
+] as InfoboxMapElement[];
 
+/**
+ * Clean up the value of the infobox
+ * @param {string} value
+ * @return {string}
+ * @example cleanUp('[[France]]') => 'France'
+*/
 function cleanUp(value: string) {
   return value.replace(/\[[0-9a-zA-Z ]+\](,\[[0-9a-zA-Z ]+\])*/g, '');
 }
 
-const handleInfoboxElementByType = (td: Element | null, infoboxData: any) => {
-  console.log('handle');
-  if (!td) return null;
-  switch (infoboxData.type) {
-    case 'listSplitByComma':
-      return td.textContent?.trim().split(', ');
-    case 'time':
-      return td.querySelector('time')?.textContent?.trim().split(', ');
-    default:
-      return null;
-  }
-};
+type HandleInfoboxElementByTypeResult = null | string[];
+type HandleInfoboxDataResult = Record<string, HandleInfoboxElementByTypeResult>;
 
-const handleInfoboxData = (infoBox: Element, infoBoxMap: any) => {
-  console.log('handle');
+/**
+ * Handle the value of the infobox by type
+ * @param {Element | null} td
+ * @param {InfoboxMapElement} infoboxElement
+ * @return {HandleInfoboxElementByTypeResult} data from the infobox element
+ * @example handleInfoboxElementByType(td, { type: 'listSplitByComma' }) => ['France', 'Belgique']
+*/
+const handleInfoboxElementByType =
+  (td: Element | null, infoboxElement: InfoboxMapElement): HandleInfoboxElementByTypeResult => {
+    if (!td) return null;
+    switch (infoboxElement.type) {
+      case 'listSplitByComma':
+        return td.textContent?.trim().split(', ') || null;
+      case 'time':
+        return td.querySelector('time')?.textContent?.trim().split(', ') || null;
+      default:
+        return null;
+    }
+  };
+
+/**
+ * Handle the infobox data
+ * @param {Element} infoBox
+ * @param {InfoboxMapElement[]} infoBoxMap
+ * @return {HandleInfoboxDataResult} data from the infobox
+*/
+const handleInfoboxData = (infoBox: Element, infoBoxMap: InfoboxMapElement[]) => {
   const trList = [...infoBox.querySelectorAll('tr')];
-  console.log(trList.length);
-  const res: any = {};
+  const res: HandleInfoboxDataResult = {};
   trList.forEach((tr) => {
-    infoBoxMap.forEach((el: any) => {
+    infoBoxMap.forEach((el) => {
       if (el.frPropNames.includes(tr.querySelector('th')?.textContent?.trim()?.toLowerCase() || '')) {
         res[el.key] = handleInfoboxElementByType(tr.querySelector('td'), el);
       }
@@ -92,117 +118,159 @@ const handleInfoboxData = (infoBox: Element, infoBoxMap: any) => {
   return res;
 };
 
+/**
+ * Extract the description of the wikipedia page
+ * @param {HTMLAllCollectionBase} elements
+ * @returns {string | null} main description of the wikipedia page
+ */
+const extractDescription = (elements: HTMLCollectionBase | undefined): string | null => {
+  if (!elements) return null;
+  let firstParagraphMatch = false;
+  const descriptionParts: string[] = [];
+  Object.keys(elements).forEach((index) => {
+    const child = elements[Number(index)];
+    if (!child) return;
+    if (child.tagName !== 'P' && firstParagraphMatch && child.textContent?.trim() !== '') return;
+    if (child.tagName === 'P' && child.textContent && child.textContent?.trim() !== '') {
+      descriptionParts.push(child.textContent);
+      firstParagraphMatch = true;
+    }
+  });
+  return cleanUp(descriptionParts.join(''));
+};
+
+const ERROR_MESSAGE_MISSING_PARAMETER = (parameterName: string) => `Missing '${parameterName}' parameter`;
+const ERROR_MESSAGE_BAD_PARAMETER_TYPE = (parameterName: string, parameterType: string) => `Query parameter '${parameterName}' must be a ${parameterType}`;
+const ERROR_MESSAGE_LOADING_WIKI_PAGE = 'Error while loading the wikipedia page';
+
+/*
+ * Scrape the wikipedia page of a person
+ * @param {functions.https.Request} request (request.query.page = url of the wikipedia page)
+ * @param {functions.https.Response} response
+ * @example https://us-central1-<project-id>.cloudfunctions.net/scrapeWikiPerson?page=https://fr.wikipedia.org/wiki/Alain_Ducasse
+ * @example local http://localhost:5001/odocapa-4a31f/us-central1/scrapeWikiPerson?page=https://fr.wikipedia.org/wiki/Alain_Ducasse
+ */
 export const scrapeWikiPerson =
   functions.https.onRequest(async (request, response) => {
     cors(request, response, async () => {
+      if (!request.query.page) {
+        response.status(400).send(ERROR_MESSAGE_MISSING_PARAMETER('page'));
+        return;
+      }
+      if (typeof request.query.page !== 'string') {
+        response.status(400).send(ERROR_MESSAGE_BAD_PARAMETER_TYPE('page', 'string'));
+        return;
+      }
       const page = request.query.page as string;
-      const { data } = await axios.get(encodeURI(page));
-      const doc = new JSDOM(data).window.document;
-      let infoBox = doc.querySelector('div.infobox_v3');
-      if (!infoBox) infoBox = doc.querySelector('table.infobox_v2');
-      console.log(infoBox);
-      const res: any = {};
-      let infoBoxData: any = {},
-      if (infoBox) {
-        infoBoxData = handleInfoboxData(infoBox, personInfoboxMap);
-      }
-      const content = doc.querySelector('div.mw-parser-output');
-      const childrens = content?.children as HTMLCollectionBase;
-      const descriptionParts = [];
-      console.log(childrens?.length);
-      let firstParagraphMatch = false;
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < childrens?.length; i++) {
-        const child = childrens[i];
-        if (child.tagName !== 'P' && firstParagraphMatch && child.textContent?.trim() !== '') {
-          break;
+      try {
+        const { data } = await axios.get(encodeURI(page));
+        const { document } = new JSDOM(data).window;
+        let infoBox = document.querySelector('div.infobox_v3');
+        if (!infoBox) infoBox = document.querySelector('table.infobox_v2');
+        const res: any = {};
+        let infoBoxData: any = {};
+        if (infoBox) {
+          infoBoxData = handleInfoboxData(infoBox, PERSON_INFOBOX_MAP);
         }
-        if (child.tagName === 'P' && child.textContent?.trim() !== '') {
-          descriptionParts.push(child.textContent);
-          firstParagraphMatch = true;
+        const descriptionParentElementDiv = 'div.mw-parser-output';
+        const directChildren = document.querySelector(descriptionParentElementDiv)?.children;
+        res.descriptionParts = extractDescription(directChildren);
+        if (res.descriptionParts.length === 0) {
+          const childrenFromTheFirstChild =
+            document.querySelectorAll(descriptionParentElementDiv)[1]?.children;
+          res.descriptionParts = extractDescription(childrenFromTheFirstChild);
         }
+        response.send({ ...res, ...infoBoxData });
+      } catch (e) {
+        response.status(400).send({ message: ERROR_MESSAGE_LOADING_WIKI_PAGE, axiosError: e });
       }
-      res.descriptionParts = cleanUp(descriptionParts?.join(''));
-      if (res.descriptionParts.length === 0) {
-        const content2 = doc.querySelectorAll('div.mw-parser-output')[1];
-        const childrens2 = content2?.children as HTMLCollectionBase;
-        const descriptionParts2 = [];
-        console.log(childrens2?.length);
-        let firstParagraphMatch2 = false;
-        // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < childrens2?.length; i++) {
-          const child = childrens2[i];
-          if (child.tagName !== 'P' && firstParagraphMatch2 && child.textContent?.trim() !== '') {
-            break;
-          }
-          if (child.tagName === 'P' && child.textContent?.trim() !== '') {
-            descriptionParts2.push(child.textContent);
-            firstParagraphMatch2 = true;
-          }
-        }
-        res.descriptionParts = cleanUp(descriptionParts2?.join(''));
-      }
-      response.send({...res, ...infoBoxData});
     });
   });
 
+/*
+ * Scrape the wikipedia page of a street
+ * @param {functions.https.Request} request (request.query.page = url of the wikipedia page)
+ * @param {functions.https.Response} response
+ * @example https://us-central1-<project-id>.cloudfunctions.net/scrapeWikiStreet?page=https://fr.wikipedia.org/wiki/Rue_de_l'Industrie_(Paris)
+ * @example local http://localhost:5001/odocapa-4a31f/us-central1/scrapeWikiStreet?page=https://fr.wikipedia.org/wiki/Rue_de_l'Industrie_(Paris)
+ */
 export const scrapeWikiStreet =
   functions.https.onRequest(async (request, response) => {
     cors(request, response, async () => {
-      const page = request.query.page as string;
-      const { data } = await axios.get(encodeURI(page));
-      const doc = new JSDOM(data).window.document;
-      const content = doc.querySelector('div#mw-content-text')?.querySelector('div.mw-parser-output');
-      const childrens = content?.children as HTMLCollectionBase;
-      let infoBox = doc.querySelector('div.infobox_v3');
-      if (!infoBox) infoBox = doc.querySelector('table.infobox_v2');
-      console.log('infoBOX', infoBox);
-      let infoBoxData = {};
-      if (infoBox) {
-        infoBoxData = handleInfoboxData(infoBox, streetInfoBoxMap);
+      if (!request.query.page) {
+        response.status(400).send(ERROR_MESSAGE_MISSING_PARAMETER('page'));
+        return;
       }
-      const history = [];
-      const nameOrigin = [];
-      const nameOriginLinks: { name: string, link: string }[] = [];
-      let mode = null;
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < childrens?.length; i++) {
-        const child = childrens[i];
-        if (mode === 'history' && child.tagName === 'P') {
-          history.push(child.textContent);
+      if (typeof request.query.page !== 'string') {
+        response.status(400).send(ERROR_MESSAGE_BAD_PARAMETER_TYPE('page', 'string'));
+        return;
+      }
+      try {
+        const page = request.query.page as string;
+        const { data } = await axios.get(encodeURI(page));
+        const doc = new JSDOM(data).window.document;
+        const content = doc.querySelector('div#mw-content-text')?.querySelector('div.mw-parser-output');
+        const childrens = content?.children as HTMLCollectionBase;
+        let infoBox = doc.querySelector('div.infobox_v3');
+        if (!infoBox) infoBox = doc.querySelector('table.infobox_v2');
+        let infoBoxData = {};
+        if (infoBox) {
+          infoBoxData = handleInfoboxData(infoBox, STREET_INFOBOX_MAP);
         }
-        if (mode === 'nameOrigin' && child.tagName === 'P') {
-          nameOrigin.push(child.textContent);
-          [...child.querySelectorAll('a')].forEach((a) => nameOriginLinks.push({ name: a.textContent || '', link: a.href }));
-        }
-        if (child.tagName === 'H2') {
-          if (child.textContent?.includes('Origine du nom')) {
-            mode = 'nameOrigin';
-          } else if (child.textContent?.includes('Historique')) {
-            mode = 'history';
-          } else {
-            mode = null;
+        const history: (string | null)[] = [];
+        const nameOrigin: (string | null)[] = [];
+        const nameOriginLinks: { name: string, link: string }[] = [];
+        let mode: string | null = null;
+        Object.keys(childrens).forEach((index) => {
+          const child = childrens[Number(index)];
+          if (mode === 'history' && child.tagName === 'P') {
+            history.push(child.textContent);
+          } else if (mode === 'nameOrigin' && child.tagName === 'P') {
+            nameOrigin.push(child.textContent);
+            [...child.querySelectorAll('a')].forEach((a) => nameOriginLinks.push({ name: a.textContent || '', link: a.href }));
+          } else if (child.tagName === 'H2') {
+            if (child.textContent?.includes('Origine du nom')) {
+              mode = 'nameOrigin';
+            } else if (child.textContent?.includes('Historique')) {
+              mode = 'history';
+            } else {
+              mode = null;
+            }
           }
-        }
+        });
+        response.send({ history: cleanUp(history.join('')), nameOrigin: cleanUp(nameOrigin.join('')), nameOriginLinks, ...infoBoxData });
+      } catch (e) {
+        response.status(400).send({ message: ERROR_MESSAGE_LOADING_WIKI_PAGE, axiosError: e });
       }
-      response.send({ history: cleanUp(history.join('')), nameOrigin: cleanUp(nameOrigin.join('')), nameOriginLinks, ...infoBoxData });
     });
   });
 
+/*
+ * Search in wikipedia
+ * @param {functions.https.Request} request (request.query.searchStr = string to search in wiki)
+ * @param {functions.https.Response} response
+ * @example https://us-central1-<project-id>.cloudfunctions.net/wikiSearch?searchStr="Roger Federer"
+ * @example local http://localhost:5001/odocapa-4a31f/us-central1/wikiSearch?page="Roger Federer"
+ */
 export const wikiSearch =
   functions.https.onRequest(async (request, response) => {
     cors(request, response, async () => {
+      if (!request.query.searchStr) {
+        response.status(400).send(ERROR_MESSAGE_MISSING_PARAMETER('searchStr'));
+        return;
+      }
+      if (typeof request.query.searchStr !== 'string') {
+        response.status(400).send(ERROR_MESSAGE_BAD_PARAMETER_TYPE('searchStr', 'string'));
+        return;
+      }
       const searchStr = request.query.searchStr as string;
-      console.log(searchStr);
       try {
         const { data } = await axios.get('https://fr.wikipedia.org/w/api.php', {
-          params: { action: 'query', format: 'json', list: 'search', srsearch: searchStr,
-          } });
+          params: { action: 'query', format: 'json', list: 'search', srsearch: searchStr },
+        });
         response.send(data);
-      } catch (error) {
-        console.log(searchStr);
-        response.send(error);
+      } catch (e) {
+        response.status(400).send({ message: ERROR_MESSAGE_LOADING_WIKI_PAGE, axiosError: e });
       }
     });
   });
-
