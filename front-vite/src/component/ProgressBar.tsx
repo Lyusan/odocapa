1daa201164db2a67fc151fc67f2b1639367c0176
lyusan <leo.champion@protonmail.com>
2023-03-07 16:38:48 +0100

    front: fix ProgressBar colors and size

--- front-vite/src/component/ProgressBar.tsx
+++ front-vite/src/component/ProgressBar.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function ProgressBar({ max, value }: { max: number; value: number }) {
   return (
-    <div className="bg-slate-100 rounded-full w-full h-8 flex relative">
+    <div className="bg-main-blue bg-opacity-60 rounded-full w-full h-7 flex relative">
       <div
-        className="bg-blue-300 m-0.5 rounded-full"
+        className="bg-main-blue m-0.5 rounded-full"
         style={{ width: `${(value / max) * 100}%` }}
       />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-slate-600">{`${value}/${max}`}</div>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm">
+        {`${value}/${max}`}
+      </div>
     </div>
   );
 }
