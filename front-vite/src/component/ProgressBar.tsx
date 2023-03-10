fbdf8e39aa4a13c71d60e5a882232a2e56f93d9f
lyusan <leo.champion@protonmail.com>
2023-03-10 12:12:54 +0100

    front: ProgressBar add transition

--- front-vite/src/component/ProgressBar.tsx
+++ front-vite/src/component/ProgressBar.tsx
@@ -4,7 +4,7 @@ export default function ProgressBar({ max, value }: { max: number; value: number
   return (
     <div className="bg-main-blue bg-opacity-60 rounded-full w-full h-7 flex relative">
       <div
-        className="bg-main-blue m-0.5 rounded-full"
+        className="bg-main-blue m-0.5 rounded-full transition-all duration-600 ease-in-out"
         style={{ width: `${(value / max) * 100}%` }}
       />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-sm">
