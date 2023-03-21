26db596e7d5a306a86cb41fbaf95083618426582
lyusan <leo.champion@protonmail.com>
2023-03-21 16:24:40 +0100

    front: fix NavigationControl padding

--- front-vite/src/component/Map/NavigationControl.tsx
+++ front-vite/src/component/Map/NavigationControl.tsx
@@ -10,13 +10,13 @@ export default function NavigationControl({
   return (
     <>
       <div
-        className="w-7 h-7 p-1 bg-white border-main-blue border-[1.5px] z-20 text-center whitespace-nowrap rounded-t-lg mb-1 font-bold text-sm cursor-pointer"
+        className="w-7 h-7 p-1 pt-0.5 bg-white border-main-blue border-[1.5px] z-20 text-center whitespace-nowrap rounded-t-lg mb-1 font-bold text-sm cursor-pointer"
         onClick={zoomIn}
       >
         +
       </div>
       <div
-        className="w-7 h-7 p-1 bg-white border-main-blue border-[1.5px] z-20 text-center whitespace-nowrap rounded-b-lg font-bold text-sm cursor-pointer"
+        className="w-7 h-7 p-1 pt-0.5 bg-white border-main-blue border-[1.5px] z-20 text-center whitespace-nowrap rounded-b-lg font-bold text-sm cursor-pointer"
         onClick={zoomOut}
       >
         -
